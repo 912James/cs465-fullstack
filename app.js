c7fed200737bc83c80c772de0fc5d29dfb9211d1
James Soto <james@Jamess-MacBook-Pro.local>
2023-09-21 22:38:52 -0400

    Create new Application API

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ require("./app_api/database/db");
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
 var travelRouter = require("./app_server/routes/travel");
+var roomsRouter = require("./app_server/routes/rooms");
+var newsRouter = require("./app_server/routes/news");
+var mealsRouter = require("./app_server/routes/meals");
+var contactRouter = require("./app_server/routes/contact");
+var aboutRouter = require("./app_server/routes/about");
 
 const apiRouter = require("./app_api/routes/index");
 
@@ -20,15 +25,20 @@ app.set("views", path.join(__dirname, "app_server", "views"));
 hbs.registerPartials(path.join(__dirname, "app_server", "views/partials"));
 app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/travel", travelRouter);
+app.use("/rooms", roomsRouter);
+app.use("/news", newsRouter);
+app.use("/meals", mealsRouter);
+app.use("/contact", contactRouter);
+app.use("/about", aboutRouter);
 
 app.use("/api", apiRouter);
 
@@ -49,5 +59,3 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
-
-
