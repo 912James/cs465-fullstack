d40c709b5cfb88b2d0258e9cf267f1ad0d057e24
James Soto <james@Jamess-MacBook-Pro.local>
2023-09-23 22:12:28 -0400

    Create new Application API

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 const tripsController = require("../controllers/trips");
--- app_server/controllers/about.js
+++ app_server/controllers/about.js
@@ -1,6 +1,6 @@
 /* GET About View */
 const about = (req, res) => {
-    pageTitle = process.env.npm_package_description + " - About"; // process.env.npm_package_description can only be accessed if you use 'npm start' *nodemon will not work
+    pageTitle = process.env.npm_package_description + " - About";
     res.render("about", { title: pageTitle });
   };
   
